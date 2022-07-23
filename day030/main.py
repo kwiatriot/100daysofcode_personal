8029a855d6fb39df78aff9abfca30b80c7023aea
kriot <wkwiatjr@me.com>
2022-07-23 13:02:56 -0600

    Adding Search button and empty function

--- day030/main.py
+++ day030/main.py
@@ -64,12 +64,16 @@ def save():
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
+# --------------------------- SEARCH PASSWORD ------------------------- #
+def find_password():
+    pass
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=200)
+canvas = Canvas(width=300, height=300)
 background_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=background_img)
 canvas.grid(column=1, row=0)
@@ -83,8 +87,8 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # Entry boxes
-website_entry = Entry(width=40)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 user_entry = Entry(width=40)
 user_entry.insert(END, string="someEmail@gmail.com")
@@ -93,6 +97,8 @@ password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3)
 
 # Buttons
+search_button = Button(text="Search", command=find_password)
+search_button.grid(column=2, row=1)
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3)
 add_button = Button(text="Add", width=38, command=save)
