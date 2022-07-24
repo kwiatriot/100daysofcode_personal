53f810a9a0c28da3be2b1d878f83fea2f0b592a4
kriot <wkwiatjr@me.com>
2022-07-24 14:16:34 -0600

    adding correct path

--- day031/main.py
+++ day031/main.py
@@ -15,15 +15,15 @@ current_word = {}
 unknown_dic = {}
 
 try:
-    data = pandas.read_csv("day031/data/words_to_learn.csv")
+    data = pandas.read_csv("/home/kriot/Code/100daysofcode_personal/day031/data/words_to_learn.csv")
 except FileNotFoundError:
-    original_data = pandas.read_csv("day031/data/french_words.csv")
-    unknown_dic = original_data.to_dict(orient="records")
+    original_data = pandas.read_csv("/home/kriot/Code/100daysofcode_personal/day031/data/french_words.csv")
+    unknown_dic = original_data.to_dict("records")
 else:
-    unknown_dic = data.to_dict(orient="records")
+    unknown_dic = data.to_dict("records")
 
 def choose_word():
-    global current_word, flip_timer
+    global current_word, flip_timer, unknown_dic
     window.after_cancel(flip_timer)
     current_word = random.choice(unknown_dic)
     canvas.itemconfig(title_text, text="French", fill="black")
@@ -39,7 +39,7 @@ def flip_card():
 def is_known():
     unknown_dic.remove(current_word)
     data = pandas.DataFrame(unknown_dic)
-    data.to_csv("day031/data/words_to_learn.csv", index=False)
+    data.to_csv("/home/kriot/Code/100daysofcode_personal/day031/data/words_to_learn.csv", index=False)
     choose_word()
 
 window = Tk()
@@ -49,16 +49,16 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
-card_front_img = PhotoImage(file="day031/images/card_front.png")
-card_back_img = PhotoImage(file="day031/images/card_back.png")
+card_front_img = PhotoImage(file="/home/kriot/Code/100daysofcode_personal/day031/images/card_front.png")
+card_back_img = PhotoImage(file="/home/kriot/Code/100daysofcode_personal/day031/images/card_back.png")
 canvas_img = canvas.create_image(400, 263, image=card_front_img)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 title_text = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 word_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 
-wrong_image = PhotoImage(file="day031/images/wrong.png")
-right_image = PhotoImage(file="day031/images/right.png")
+wrong_image = PhotoImage(file="/home/kriot/Code/100daysofcode_personal/day031/images/wrong.png")
+right_image = PhotoImage(file="/home/kriot/Code/100daysofcode_personal/day031/images/right.png")
 
 wrong_button = Button(image=wrong_image, highlightthickness=0, command=choose_word)
 wrong_button.grid(row=1, column=0)
