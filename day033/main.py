604d88e699faaa1135221a24b945cb4509c0a47c
kriot <wkwiatjr@me.com>
2022-07-26 15:28:31 -0600

    Added main program

--- day033/main.py
+++ day033/main.py
@@ -5,3 +5,54 @@ Date: 7/26/2022
 Working with API
 """
 
+from multiprocessing import connection
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_LAT = 39.986771 # Your latitude
+MY_LONG = -104.812599 # Your longitude
+MY_EMAIL = "kriotinfo@gmail.com"
+MY_PASS = ""  #ADD PASSWORD HERE FOR ACCOUNT USE
+
+def overhead_check():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    #Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
+
+def night_check():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get(f"https://api.sunrise-sunset.org/json?lat={MY_LAT}&lng={MY_LONG}", params=parameters, verify=False)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+while True:
+    time.sleep(60)
+    if overhead_check() and night_check():
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASS)
+            connection.sendmail(
+                from_addr=MY_EMAIL, 
+                to_addrs=MY_EMAIL, 
+                msg=f"Subject:Look UP!\n\nThe ISS is above you in the sky!"
+                )
