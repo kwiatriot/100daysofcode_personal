3f484a4f2bbe9f5a6910dc50d0c0e27de2ff848a
kriot <wkwiatjr@me.com>
2022-07-27 12:22:19 -0600

    Updating structure to fill UI

--- day034/ui.py
+++ day034/ui.py
@@ -1,11 +1,13 @@
-from time import clock_getres
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -17,7 +19,8 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
         self.quiz_text = self.canvas.create_text(
             150, 
-            125, 
+            125,
+            width=280,
             text="Place holder", 
             font=("Ariel", 18, "italic"), 
             fill=THEME_COLOR,
@@ -31,4 +34,10 @@ class QuizInterface:
         self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.quiz_text, text=q_text)
